10200eadaf7ab48d724e7b87535c9ac8f48b99a5
ronan-alt <ridel.ronan@gmail.com>
2026-01-06 09:19:33 +0100

    throw 1

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <meta charset="UTF-8">
+    <meta name="description" content="Page de vente en ligne de dynamite games">
+    <meta name="keywords" content="jeux video, nintendo, xbox,console, console de jeu,xcube,psp,psp2,playstation,playstation2,sony,microsoft,atari">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dynamite Games Webshop - Jeux Video . Tests . Vente . Achat . Occasion</title>
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</html>
