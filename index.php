7a26297b8574d7ef7a773478fd6b1e1f0a04d999
ronan-alt <ridel.ronan@gmail.com>
2026-01-06 12:12:25 +0100

    throw 2

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!doctype html>
 <html>
-    <meta charset="UTF-8">
-    <meta name="description" content="Page de vente en ligne de dynamite games">
-    <meta name="keywords" content="jeux video, nintendo, xbox,console, console de jeu,xcube,psp,psp2,playstation,playstation2,sony,microsoft,atari">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Dynamite Games Webshop - Jeux Video . Tests . Vente . Achat . Occasion</title>
-    <link rel="icon" type="image/png" href="favicon.png" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Page de vente en ligne de dynamite games">
+        <meta name="keywords" content="jeux video, nintendo, xbox,console, console de jeu,xcube,psp,psp2,playstation,playstation2,sony,microsoft,atari">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Dynamite Games Webshop - Jeux Video . Tests . Vente . Achat . Occasion</title>
+        <link rel="icon" type="image/png" href="favicon.png" />
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <button onclick="topFunction()" id="topBtn" style="background: rgba(39, 56, 70, 0.3);" 
+        class="pointer-events-auto rounded-full p-2 w-12 h-12 flex items-center justify-center 
+        hover:opacity-80 transition-all duration-200 z-20 relative">flex
+    </body>
 </html>
